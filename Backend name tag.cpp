3de9f3ff136e1408d3706225e0f3bc7ce730e7f5
Jocelinda <111058466+Jocelinda@users.noreply.github.com>
2022-10-06 22:51:04 +0700

    Add files via upload

---
+++ Backend name tag.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	cout<<"Ceritakan alasan kamu masuk jurusan informatik UBM!"<<endl;
+	cout<<"1. Lokasi kampus dekat rumah"<<endl;
+	cout<<"2. Harga semester lebih murah"<<endl;
+	cout<<"3. Merasa jurusan informatik cukup menjamin di UBM"<<endl;
+}
+
+
---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+	int Alas;
+	int Tinggi;
+	int Luas= Alas * Tinggi;
+	
+	cout<<"Masukkan Alas=\n"<<endl;
+	cin>>Alas; 
+	cout<<"Masukkan Tinggi\n"<<endl; 
+	cin>>Tinggi;
+	cout<< "Hasil dari A dikali T adalah"
+	return 0;
+	
+}
